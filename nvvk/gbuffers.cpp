afb1cc96fb685bbd0cf11b58cd12cfe1c9c817ca
DerThomy <t.koehler@student.tugraz.at>
2026-02-04 14:23:17 +0100

    Added support for TAA

--- nvvk/gbuffers.cpp
+++ nvvk/gbuffers.cpp
@@ -176,7 +176,7 @@ VkResult nvvk::GBuffer::initResources(VkCommandBuffer cmd)
   {
     // Color image and view
     const VkImageUsageFlags usage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT
-                                    | VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT;
+                                    | VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_STORAGE_BIT;
     VkImageCreateInfo info = {
         .sType       = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
         .imageType   = VK_IMAGE_TYPE_2D,
